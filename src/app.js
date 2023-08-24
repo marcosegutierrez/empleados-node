e5b51ed2ba40a48aeaaf8c4f5981b18862193112
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-08-23 21:02:41 -0300

    Ruta 404 not found

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use('/api',employeesRoutes);
 app.use(indexRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).json({message: 'Error 404, ruta no encontrada'});
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server corriendo en el puerto ${PORT} -> http://localhost:${PORT}`);
