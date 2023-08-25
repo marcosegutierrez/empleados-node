469cced27608f99054d21fffaaba4c2d35d7b2e5
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-08-24 22:42:44 -0300

    Se agrega puerto de BD

--- src/db.js
+++ src/db.js
@@ -5,10 +5,12 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PORT = process.env.DB_PORT;
 
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_DATABASE,
+    port: DB_PORT
 });
